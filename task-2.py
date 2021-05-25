b76fd0a5e266e42eb923e1a89bb5f06ef5b0d686
Nikolos123 <Isponec2>
2021-05-25 17:01:06 +0300

    task-2

---
+++ task-2.py
@@ -0,0 +1,17 @@
+# Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность
+# кодов (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
+
+def main():
+    worlds =  ['class','function','method']
+
+    for i in worlds:
+        print(f'имя : {bytes(i,encoding="utf-8")}')
+        print(f'тип : {type(bytes(i,encoding="utf-8"))}')
+
+
+
+if __name__ =='__main__':
+    try:
+        main()
+    except Exception as ans:
+        print(ans)
