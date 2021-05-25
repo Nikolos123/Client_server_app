9d6af9fb9dd970f833ea7d55c9bc05242735170d
Nikolos123 <Isponec2>
2021-05-25 22:21:42 +0300

    task-6

---
+++ task-6.py
@@ -0,0 +1,23 @@
+# Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор».
+# Проверить кодировку файла по умолчанию.
+# Принудительно открыть файл в формате Unicode и вывести его содержимое
+import locale
+
+
+def main():
+    world = ['сетевое программирование', 'сокет', 'декоратор']
+    with open('test_file.txt', 'w') as ans:
+        for i in world:
+            ans.write(f'{i}\n')
+
+    file_coding = locale.getdefaultlocale()
+
+    with open('test_file.txt', 'r', encoding=file_coding[1]) as f:
+        print(f.read())
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as ans:
+        print(ans)
