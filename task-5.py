6431897fa93e57d1479a1aaeea68467128f1ea1e
Nikolos123 <Isponec2>
2021-05-25 22:00:18 +0300

    task-5

---
+++ task-5.py
@@ -0,0 +1,23 @@
+# Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из
+# байтовового в строковый тип на кириллице.
+
+import subprocess
+
+def main():
+    all_list = []
+    all_list.append(['ping', 'yandex.com'])
+    all_list.append(['ping', 'youtube.com'])
+
+    for i in all_list:
+        subproc_ping = subprocess.Popen(i,stdout=subprocess.PIPE)
+        for line in subproc_ping.stdout:
+            print(line.decode(encoding='cp866'))
+
+
+
+
+if __name__ =='__main__':
+    try:
+        main()
+    except Exception as ans:
+        print(ans)
