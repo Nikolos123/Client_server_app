537de6be914388d2967d5cad1dcd394b249c82e6
Nikolos123 <Isponec2>
2021-06-26 22:19:42 +0300

    update Homework-7

--- Homework-7/client.py
+++ Homework-7/client.py
@@ -1,24 +1,17 @@
 from Client_models import *
 import logging
 import log.client_log_config
-from socket import AF_INET, SOCK_STREAM, socket, SOL_SOCKET, SO_REUSEADDR
+from socket import AF_INET, SOCK_STREAM, socket
 
 logger = logging.getLogger('client')
 from deco import logdec
 
 
-def socet_con():
-    adr = ('localhost',7777)
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        logger.info(f'Инициализация сокета прошла успешна.')
-        return sock.connect(adr)
-
 @logdec
 def client() -> None:
-    adr = ('localhost',7777)
     with socket(AF_INET, SOCK_STREAM) as soc:
         logger.info(f'Инициализация сокета прошла успешна.')
-        soc.connect(('localhost',7777))
+        soc.connect(('localhost', 7777))
         start_client = ClientSocket()
         while True:
 
@@ -30,14 +23,14 @@ def client() -> None:
                 message = input(' Введите сообщение ')
                 logger.info(f'Начало инициализации отправки сообщения')
                 logger.info(f'Отправка данных на сервер для отправки сообщения')
-                start_client.client(message, '', '', ans,soc)
+                start_client.client(message, '', '', ans, soc)
             elif ans == '2':
                 logger.info(f'Начало инициализации отправки сообщения')
                 logger.info(f'Получить ответ на сообщения')
-                start_client.client('', '', '', ans,soc)
+                start_client.client('', '', '', ans, soc)
             elif ans == '3':
                 logger.info(f'Начинаем закрывать соединение')
-                start_client.client('', '', '', ans,soc)
+                start_client.client('', '', '', ans, soc)
             else:
                 logger.error(f'Выбранный вариант ответа не существует в системе ')
 
